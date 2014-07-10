057bb5b436d4dcf4426498777105a04ea488313d
John Johnson <segrived@gmail.com>
2014-07-10 20:06:01 +0300

    Init version

---
+++ SimpleImageEditor/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace SimpleImageEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageArea = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.invertImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveImageDialog = new System.Windows.Forms.SaveFileDialog();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.imageArea)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageArea
+            // 
+            this.imageArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageArea.Location = new System.Drawing.Point(0, 24);
+            this.imageArea.Name = "imageArea";
+            this.imageArea.Size = new System.Drawing.Size(514, 332);
+            this.imageArea.TabIndex = 0;
+            this.imageArea.TabStop = false;
+            this.imageArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imageArea_MouseDown);
+            this.imageArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.imageArea_MouseMove);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(514, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.invertImageToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.editToolStripMenuItem.Text = "Adjustments";
+            // 
+            // invertImageToolStripMenuItem
+            // 
+            this.invertImageToolStripMenuItem.Name = "invertImageToolStripMenuItem";
+            this.invertImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.invertImageToolStripMenuItem.Text = "Invert colors";
+            this.invertImageToolStripMenuItem.Click += new System.EventHandler(this.invertImageToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveImageDialog
+            // 
+            this.saveImageDialog.DefaultExt = "png";
+            this.saveImageDialog.Filter = "PNG|*.png";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 356);
+            this.Controls.Add(this.imageArea);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Simple Image Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.imageArea)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imageArea;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem invertImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveImageDialog;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ SimpleImageEditor/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SimpleImageEditor
+{
+    public partial class MainForm : Form
+    {
+        private Point? _prevPoint;
+        public MainForm()
+        {
+            InitializeComponent();
+            var bitmap = new Bitmap(imageArea.Width, imageArea.Height);
+            using (var g = Graphics.FromImage(bitmap)) {
+                g.FillRectangle(Brushes.White, new Rectangle(0, 0, imageArea.Width, imageArea.Height));
+            }
+            imageArea.Image = bitmap;
+            _prevPoint = null;
+        }
+
+        private void imageArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) {
+                if (_prevPoint.HasValue) {
+                    using (var g = Graphics.FromImage(imageArea.Image)) {
+                        g.DrawLine(new Pen(Color.Black), _prevPoint.Value, new Point(e.X, e.Y));
+                    }
+                    imageArea.Invalidate();
+                }
+                _prevPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void imageArea_MouseDown(object sender, MouseEventArgs e)
+        {
+            _prevPoint = null;
+        }
+
+        private void invertImageToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            var image = (Bitmap)imageArea.Image;
+            for (int i = 0; i < image.Width; i++) {
+                for (int j = 0; j < image.Height; j++) {
+                    var current = image.GetPixel(i, j);
+                    var r = 255 - current.R;
+                    var g = 255 - current.G;
+                    var b = 255 - current.B;
+                    image.SetPixel(i, j, Color.FromArgb(r, g, b));
+                }
+            }
+            imageArea.Image = image;
+            imageArea.Invalidate();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            var res = saveImageDialog.ShowDialog();
+            if (res == DialogResult.OK) {
+                imageArea.Image.Save(saveImageDialog.FileName);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
